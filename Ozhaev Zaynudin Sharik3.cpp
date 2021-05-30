cf85579b3802e06108899d20532afba897b8d21f
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-05-31 00:51:58 +0300

    C:\Users\zainu\Desktop\курс\proekt2\Ozhaev Zaynudin Sharik3.cpp -o C:\Users\zainu\Desktop\курс\proekt2\Ozhaev Zaynudin Sharik3.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin Sharik3.cpp
+++ Ozhaev Zaynudin Sharik3.cpp
@@ -28,8 +28,8 @@ void MpveBall()
 
         txCircle (x, y, r);
 
-        txLine (x, y, x + vx*5 + r, y + vy*5 + r);
-        txCircle (x + vx*5 + r, y + vy*5 + r, 3);
+        txLine (x, y, x + vx*5, y + vy*5);
+        txCircle (x + vx*5, y + vy*5, 3);
 
         x += vx * dt;
         y += vy * dt;
