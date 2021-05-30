da83ca514d8676b6afe85624e192be99f2759073
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-05-31 02:58:49 +0300

    C:\Users\zainu\Desktop\курс\proekt2\Ozhaev Zaynudin Sharik3.cpp -o C:\Users\zainu\Desktop\курс\proekt2\Ozhaev Zaynudin Sharik3.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin Sharik3.cpp
+++ Ozhaev Zaynudin Sharik3.cpp
@@ -45,7 +45,7 @@ void MoveBall()
         DrowBall(x1, y1, vx1, vy1, r1, TX_LIGHTBLUE , TX_BLUE );
         DrowBall(x2, y2, vx2, vy2, r2, TX_LIGHTRED  , TX_RED  );
         DrowBall(x3, y3, vx3, vy3, r3, TX_LIGHTGREEN, TX_GREEN);
-        DrowBall(x4, y4, vx4, vy4, r4, TX_LIGHTBROWN, TX_BROWN);
+        DrowBall(x4, y4, vx4, vy4, r4, TX_TX_PINK   , TX_BROWN);
 
         PhysicsBall(&x1, &y1, &vx1, &vy1, dt, r1);
         PhysicsBall(&x2, &y2, &vx2, &vy2, dt, r2);
