b2b706cc57f6452e9623bb8152a02a5469eb793b
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-06-09 17:25:58 +0300

    C:\Users\zainu\Desktop\курс\proekt2\Ozhaev Zaynudin Sharik2.cpp -o C:\Users\zainu\Desktop\курс\proekt2\Ozhaev Zaynudin Sharik2.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin Sharik2.cpp
+++ Ozhaev Zaynudin Sharik2.cpp
@@ -32,8 +32,8 @@ void MoveBall()
 
         txCircle (x1, y1, r1);
 
-        x1 += vx1 * dt;
-        y1 += vy1 * dt;
+        //x1 += vx1 * dt;
+       // y1 += vy1 * dt;
 
         if (x1 > 800 - r1)
             {
@@ -64,8 +64,8 @@ void MoveBall()
 
         txCircle (x2, y2, r2);
 
-        x2 += vx2 * dt;
-        y2 += vy2 * dt;
+       // x2 += vx2 * dt;
+        //y2 += vy2 * dt;
 
         if (x2 > 800 - r2)
             {
@@ -90,6 +90,49 @@ void MoveBall()
              vy2 = -vy2;
              y2 = 0 + r2;
             }
+
+            float Dx = x1 - x2;
+      	float Dy = y1 - y2;
+      	float d = sqrt(Dx*Dx+Dy*Dy); if (d == 0) d = 0.01;
+      	float s = Dx/d; // sin
+      	float e = Dy/d; // cos
+      	if (d < r1+r2){
+      		float Vn1 = dx2*s + dy2*e;
+      		float Vn2 = dx1*s + dy1*e;
+      		float dt = (r2+r1-d)/(Vn1-Vn2);
+      		if (dt > 0.6) dt = 0.6;
+      		if (dt < -0.6) dt = -0.6;
+      		x1-=dx1*dt;
+      		y1-=dy1*dt;
+      		x2-=dx2*dt;
+      		y2-=dy2*dt;
+      		Dx = x1 - x2;
+      		Dy = y1 - y2;
+      		d = sqrt(Dx*Dx+Dy*Dy); if (d == 0) d = 0.01;
+      		s = Dx/d; // sin
+      		e = Dy/d; // cos
+      		Vn1 = dx2*s + dy2*e;
+      		Vn2 = dx1*s + dy1*e;
+      		float Vt1 = -dx2*e+dy2*s;
+      		float Vt2 = -dx1*e+dy1*s;
+
+
+      		float o = Vn2;
+      		Vn2 = Vn1;
+      		Vn1 = o;
+
+
+      		dx1 = Vn2*s-Vt2*e;
+      		dy1 = Vn2*e+Vt2*s;
+      		dx2 = Vn1*s-Vt1*e;
+      		dy2 = Vn1*e+Vt1*s;
+      		x1+=dx1*dt;
+      		y1+=dy1*dt;
+      		x2+=dx2*dt;
+      		y2+=dy2*dt;
+      	}
+
+
 
         if (txGetAsyncKeyState (VK_RIGHT)) vx1++;
         if (txGetAsyncKeyState (VK_LEFT))  vx1--;
