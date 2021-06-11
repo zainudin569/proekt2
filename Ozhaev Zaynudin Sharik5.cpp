1ef291c595374a81f225a83a40f90e95a94ab8c8
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-06-11 13:15:16 +0300

    C:\Users\zainu\Desktop\курс\proekt2\Ozhaev Zaynudin Sharik5.cpp -o C:\Users\zainu\Desktop\курс\proekt2\Ozhaev Zaynudin Sharik5.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin Sharik5.cpp
+++ Ozhaev Zaynudin Sharik5.cpp
@@ -225,13 +225,13 @@ void CollisionBall (Ball* ball_1, Ball* ball_2, int dt)
         float Vn1 = (*ball_2) .vx*sin + (*ball_2) .vy*cos; //поворот системы координат шар1
         float Vn2 = (*ball_1) .vx*sin + (*ball_1) .vy*cos; //поворот системы координат шар2
 
-        float vt = ((*ball_1) .r + (*ball_2) .r - d)/(Vn1 - Vn2);
-      		if (dt > 0.6) dt = 0.6;
-      		if (dt < -0.6) dt = -0.6;
-      		(*ball_1) .x -=(*ball_1) .vx*vt;
-      		(*ball_1) .y -=(*ball_1) .vy*vt;
-      		(*ball_2) .x -=(*ball_2) .vx*vt;
-      		(*ball_2) .y -=(*ball_2) .vy*vt;
+        float vt = ((*ball_1) .r + (*ball_2) .r - d)/(Vn1 - Vn2); // удаление залипания
+      		//if (dt > 0.6) dt = 0.6;
+      		//if (dt < -0.6) dt = -0.6;
+      		(*ball_1) .x = (*ball_1) .x - (*ball_1) .vx*vt;
+      		(*ball_1) .y = (*ball_1) .x - (*ball_1) .vy*vt;
+      		(*ball_2) .x = (*ball_2) .x - (*ball_2) .vx*vt;
+      		(*ball_2) .y = (*ball_2) .x - (*ball_2) .vy*vt;
       		Dx = (*ball_1) .x - (*ball_2) .x;
       		Dy = (*ball_1) .y - (*ball_2) .y;
       		d = sqrt(Dx*Dx+Dy*Dy); if (d == 0) d = 0.01;
