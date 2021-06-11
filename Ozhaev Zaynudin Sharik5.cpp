c1196130690124a1fe9269c5bc9a8b1e8b3762fa
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-06-11 14:26:12 +0300

    C:\Users\zainu\Desktop\курс\proekt2\Ozhaev Zaynudin Sharik5.cpp -o C:\Users\zainu\Desktop\курс\proekt2\Ozhaev Zaynudin Sharik5.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin Sharik5.cpp
+++ Ozhaev Zaynudin Sharik5.cpp
@@ -237,8 +237,8 @@ void CollisionBall (Ball* ball_1, Ball* ball_2, int dt)
 
         float Vt = ((*ball_1) .r + (*ball_2) .r - d)/(Vn1 - Vn2); // удаление залипания
 
-        if (Vt > 0.6) vt = 0.6;
-        if (Vt < -0.6) vt = -0.6;
+        if (Vt > 0.6) Vt = 0.6;
+        if (Vt < -0.6) Vt = -0.6;
 
         printf ("Coordin do(): r1  = %d, r2= %d, vt  = %d\n", (*ball_1) .r, (*ball_2) .r, Vt);
         printf ("Coordin do(): vn1  = %d and vn2  = %d\n", Vn1, Vn2);
