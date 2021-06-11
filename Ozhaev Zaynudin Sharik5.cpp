bf1922728808101b183b2de4f3e3e4b996116bc7
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-06-11 12:24:08 +0300

    C:\Users\zainu\Desktop\курс\proekt2\Ozhaev Zaynudin Sharik5.cpp -o C:\Users\zainu\Desktop\курс\proekt2\Ozhaev Zaynudin Sharik5.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin Sharik5.cpp
+++ Ozhaev Zaynudin Sharik5.cpp
@@ -205,8 +205,8 @@ viod Bam_balls (int* x, int* y, int* vx, int* vy);
 
 void CollisionBall (Ball* ball_1, Ball* ball_2, int dt)
 {
-    float Dx = *ball_1 .x - *ball_1 .x; // стороны треугольника
-    float Dy = *ball_1 .y - *ball_1 .y; // стороны треугольника
+    float Dx = (*ball_1) .x - (*ball_1) .x; // стороны треугольника
+    float Dy = (*ball_1) .y - (*ball_1) .y; // стороны треугольника
     float d = sqrt(Dx*Dx + Dy*Dy); if (d == 0) d = 0.01; //гипотенуза
     float sin = Dx/d; // sin угла треугольника
     float cos = Dy/d; // cos угла треугольника
