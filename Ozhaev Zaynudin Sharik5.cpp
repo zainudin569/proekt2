82994e6bcff8310b00abddd78387407b0094cead
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-06-11 12:23:26 +0300

    C:\Users\zainu\Desktop\курс\proekt2\Ozhaev Zaynudin Sharik5.cpp -o C:\Users\zainu\Desktop\курс\proekt2\Ozhaev Zaynudin Sharik5.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin Sharik5.cpp
+++ Ozhaev Zaynudin Sharik5.cpp
@@ -205,32 +205,32 @@ viod Bam_balls (int* x, int* y, int* vx, int* vy);
 
 void CollisionBall (Ball* ball_1, Ball* ball_2, int dt)
 {
-    float Dx = (*ball_1) .x - (*ball_2) .x; // стороны треугольника
-    float Dy = (*ball_1) .y - (*ball_2) .y; // стороны треугольника
+    float Dx = *ball_1 .x - *ball_1 .x; // стороны треугольника
+    float Dy = *ball_1 .y - *ball_1 .y; // стороны треугольника
     float d = sqrt(Dx*Dx + Dy*Dy); if (d == 0) d = 0.01; //гипотенуза
     float sin = Dx/d; // sin угла треугольника
     float cos = Dy/d; // cos угла треугольника
 
-    if (d < (*ball_1) .r + (*ball_2) .r) //проверка столкновения
+    if (d < *ball_1 .r + *ball_2 .r) //проверка столкновения
         {
-        float (*ball_1) .Vn = (*ball_2) .vx*sin + (*ball_2) .vy*cos; //поворот системы координат шар1
-        float (*ball_2) .Vn = (*ball_1) .vx*sin + (*ball_1) .vy*cos; //поворот системы координат шар2
-        float (*ball_1) .Vt = -(*ball_2) .vx*cos + (*ball_2) .vy*sin; //поворот системы координат шар1
-        float (*ball_2) .Vt = -(*ball_1) .vx*cos + (*ball_1) .vy*sin; //поворот системы координат шар2
+        float Vn1 = (*ball_2) .vx*sin + (*ball_2) .vy*cos; //поворот системы координат шар1
+        float Vn2 = (*ball_1) .vx*sin + (*ball_1) .vy*cos; //поворот системы координат шар2
+        float Vt1 = -(*ball_2) .vx*cos + (*ball_2) .vy*sin; //поворот системы координат шар1
+        float Vt2 = -(*ball_1) .vx*cos + (*ball_1) .vy*sin; //поворот системы координат шар2
 
-        float o = *(ball_2) .Vn; //меняем местами vn1 и vn2
-        *(ball_2) .Vn = (*ball_1) .Vn;
-        (*ball_1) .Vn = o;
+        float o = Vn2; //меняем местами vn1 и vn2
+        Vn2 = Vn1;
+        Vn1 = o;
 
-        (*ball_1) .vx = *(ball_2) .Vn*sin - *(ball_2) .Vt*cos; //обратный поворот системы координат шар1
-        (*ball_1) .vy = *(ball_2) .Vn*cos + *(ball_2) .Vt*sin; //обратный поворот системы координат шар1
-        *(ball_2) .vx = (*ball_1) .Vn*sin - (*ball_1) .Vt*cos; //обратный поворот системы координат шар2
-        *(ball_2) .vy = (*ball_1) .Vn*cos + (*ball_1) .Vt*sin; //обратный поворот системы координат шар2
+        (*ball_1) .vx = Vn2*sin - Vt2*cos; //обратный поворот системы координат шар1
+        (*ball_1) .vy = Vn2*cos + Vt2*sin; //обратный поворот системы координат шар1
+        (*ball_2) .vx = Vn1*sin - Vt1*cos; //обратный поворот системы координат шар2
+        (*ball_2) .vy = Vn1*cos + Vt1*sin; //обратный поворот системы координат шар2
 
         (*ball_1) .x = (*ball_1) .x + (*ball_1) .vx*dt;
         (*ball_1) .y = (*ball_1) .y + (*ball_1) .vy*dt;
-        *(ball_2) .x = *(ball_2) .x + *(ball_2) .vx*dt;
-        *(ball_2) .y = *(ball_2) .y + *(ball_2) .vy*dt;
+        (*ball_2) .x = (*ball_2) .x + (*ball_2) .vx*dt;
+        (*ball_2) .y = (*ball_2) .y + (*ball_2) .vy*dt;
         }
 
 }
