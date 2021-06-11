589e71bd40c04ee7e3039ed1bdc7aa2166067256
zainudin569 <81910959+zainudin569@users.noreply.github.com>
2021-06-11 15:46:13 +0300

    C:\Users\zainu\Desktop\курс\proekt2\Ozhaev Zaynudin Sharik5.cpp -o C:\Users\zainu\Desktop\курс\proekt2\Ozhaev Zaynudin Sharik5.exe  -Wshadow -Winit-self -Wredundant-decls -Wcast-align -Wundef -Wfloat-equal -Winline -Wunreachable-code -Wmissing-declarations -Wmissing-include-dirs -Wswitch-enum -Wswitch-default -Weffc++ -Wmain -Wextra -Wall -g -pipe -fexceptions -Wcast-qual -Wconversion -Wctor-dtor-privacy -Wempty-body -Wformat-security -Wformat=2 -Wignored-qualifiers -Wlogical-op -Wmissing-field-initializers -Wnon-virtual-dtor -Woverloaded-virtual -Wpointer-arith -Wsign-promo -Wstack-usage=8192 -Wstrict-aliasing -Wstrict-null-sentinel -Wtype-limits -Wwrite-strings -D_DEBUG -D_EJUDGE_CLIENT_SIDE -D_EJC

--- Ozhaev Zaynudin Sharik5.cpp
+++ Ozhaev Zaynudin Sharik5.cpp
@@ -86,8 +86,8 @@ void MoveBall()
 
 
         CollisionBall(&ball1, &ball2, dt);
-        CollisionBall(&ball1, &ball3, dt);
-        CollisionBall(&ball1, &ball4, dt);
+        //CollisionBall(&ball1, &ball3, dt);
+        //CollisionBall(&ball1, &ball4, dt);
 
         /*CollisionBall(&ball2, &ball1, dt);
         CollisionBall(&ball2, &ball3, dt);
@@ -242,7 +242,7 @@ void CollisionBall (Ball* ball_1, Ball* ball_2, int dt)
 
         double Vt = ((*ball_1) .r + (*ball_2) .r - d)/(Vn1 - Vn2); // удаление залипания
 
-        if (Vt > 0.6) Vt = 0.6;
+        if (Vt > 0.6) Vt = 0.6
         if (Vt < -0.6) Vt = -0.6;
 
         printf ("Coordin do(): r1  = %d, r2= %d, vt  = %f\n", (*ball_1) .r, (*ball_2) .r, Vt);
